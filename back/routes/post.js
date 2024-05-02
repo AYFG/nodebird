01fb3f976da6be2ce90553beffd873695080d988
AYFG <sek82468246@gmail.com>
2024-05-02 18:04:51 +0900

    this.client.send is not a function 오류 해결중

--- back/routes/post.js
+++ back/routes/post.js
@@ -24,7 +24,7 @@ AWS.config.update({
 const upload = multer({
   storage: multerS3({
     s3: new AWS.S3(),
-    bucket: "react-woodbird-s3",
+    bucket: "react-nodebird",
     key(req, file, cb) {
       cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     },
