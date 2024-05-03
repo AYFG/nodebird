df3b4f14ac4dd7f8e33ac778cd32812801c1203f
AYFG <sek82468246@gmail.com>
2024-05-03 19:36:35 +0900

    prefetch - false

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -135,7 +135,7 @@ export const PostCard = ({ post }) => {
             </div>
             <Card.Meta
               avatar={
-                <Link href={`/user/${post.Retweet.User.id}`}>
+                <Link href={`/user/${post.Retweet.User.id}`} prefetch={false}>
                   <a>
                     <Avatar>{post.Retweet.User.nickname[0]}</Avatar>
                   </a>
@@ -152,7 +152,7 @@ export const PostCard = ({ post }) => {
             </div>
             <Card.Meta
               avatar={
-                <Link href={`/user/${post.User.id}`}>
+                <Link href={`/user/${post.User.id}`} prefetch={false}>
                   <a>
                     <Avatar>{post.User.nickname[0]}</Avatar>
                   </a>
@@ -176,7 +176,7 @@ export const PostCard = ({ post }) => {
                 <Comment
                   author={item.User.nickname}
                   avatar={
-                    <Link href={`/user/${post.User.id}`}>
+                    <Link href={`/user/${post.User.id}`} prefetch={false}>
                       <a>
                         <Avatar>{post.User.nickname[0]}</Avatar>
                       </a>
